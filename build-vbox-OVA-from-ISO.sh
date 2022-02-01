7aee2dbff091f0edf95a047c828e4df73820b81c
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 20:00:28 +0200

    build-vbox-OVA-from-ISO.sh: Download the hardcoded URL

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+ISO_URL="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
+
+wget -nv --show-progress -c --content-disposition ${ISO_URL}
