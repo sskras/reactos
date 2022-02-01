d3cc553473ae464075972268bcfcc67ef1dea32c
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 22:55:40 +0200

    build-vbox-OVA-from-ISO.sh: Print using a helper function

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -16,11 +16,11 @@ ISO_ZIP="${ISO_ZIP##*/}"
 # TODO: either use ${ISO_FILE} as the made up output filename,
 #       or just calculat it from the `wget` output in some way:
 
-echo; echo - Retrieving:
+print - Retrieving:
 wget -nv --show-progress -c --content-disposition ${ISO_URL}
 ls -l "${ISO_ZIP}"
 
-echo; echo - Extracting:
+print - Extracting:
 bsdtar -tvf ${ISO_ZIP} \
         | awk '/iso$/ {$1=$2=$3=$4=$5=$6=$7=$8=""; print}' \
         | read ISO_FILE
@@ -28,15 +28,15 @@ bsdtar -xvkf ${ISO_ZIP} \
         |& grep --color -e ^ -e "${ISO_FILE}"
 ls -l "${ISO_FILE}"
 
-echo; echo - Creating VM:
+print - Creating VM:
 VBoxManage list vms
 VBoxManage createvm --name ${VM_NAME} --ostype "Windows2003" --basefolder VMs/ --register
 
-echo; echo - Listing VMs:
+print - Listing VMs:
 VBoxManage list vms
 
 print "Press <Enter> to finish"
 read
 
-echo; echo - Destroying VM:
+print - Destroying VM:
 VBoxManage unregistervm ${VM_NAME} --delete
