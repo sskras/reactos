08a4c692ea31cfe9225388324c7219eb992966b8
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 22:45:42 +0200

    build-vbox-OVA-from-ISO.sh: Build VM, destroy VM

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ISO_URL="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
+VM_NAME="ReactOS-0.4.14-LiveCD"
 
 shopt -s lastpipe
 
@@ -9,14 +10,20 @@ ISO_ZIP_="${ISO_ZIP_##*/}"
 # TODO: either use ${ISO_FILE} as the made up output filename,
 #       or just calculat it from the `wget` output in some way:
 
-echo; echo Retrieving:
+echo; echo - Retrieving:
 wget -nv --show-progress -c --content-disposition ${ISO_URL}
 ls -l "${ISO_ZIP_}"
 
-echo; echo Extracting:
+echo; echo - Extracting:
 bsdtar -tvf ${ISO_ZIP_} \
         | awk '/iso$/ {$1=$2=$3=$4=$5=$6=$7=$8=""; print}' \
         | read ISO_FILE
 bsdtar -xvkf ${ISO_ZIP_} \
         |& grep --color -e ^ -e "${ISO_FILE}"
 ls -l "${ISO_FILE}"
+
+echo; echo - Creating VM:
+VBoxManage createvm --name ${VM_NAME} --ostype "Windows2003" --basefolder VMs/ --register
+
+echo; echo - Destroying VM:
+VBoxManage unregistervm ${VM_NAME} --delete
