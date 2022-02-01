091be0ec1a2415afd455ae38b6c082d891d37c3f
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 22:25:40 +0200

    build-vbox-OVA-from-ISO.sh: Use a better name

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -2,9 +2,9 @@
 
 ISO_URL_="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
 
-ISO_FILE="${ISO_URL_%/*}"
-ISO_FILE="${ISO_FILE##*/}"
+ISO_ZIP_="${ISO_URL_%/*}"
+ISO_ZIP_="${ISO_ZIP_##*/}"
 # TODO: either use ${ISO_FILE} as the made up output filename,
 #       or just calculat it from the `wget` output in some way:
 wget -nv --show-progress -c --content-disposition ${ISO_URL_}
-ls -l "${ISO_FILE}"
+ls -l "${ISO_ZIP_}"
