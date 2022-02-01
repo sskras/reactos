f95a5b0c7a147bc00e24129d32e7a38c49a3d2c5
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 22:37:40 +0200

    build-vbox-OVA-from-ISO.sh: Still a better name

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-ISO_URL_="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
+ISO_URL="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
 
 shopt -s lastpipe
 
-ISO_ZIP_="${ISO_URL_%/*}"
+ISO_ZIP_="${ISO_URL%/*}"
 ISO_ZIP_="${ISO_ZIP_##*/}"
 # TODO: either use ${ISO_FILE} as the made up output filename,
 #       or just calculat it from the `wget` output in some way:
 
 echo; echo Retrieving:
-wget -nv --show-progress -c --content-disposition ${ISO_URL_}
+wget -nv --show-progress -c --content-disposition ${ISO_URL}
 ls -l "${ISO_ZIP_}"
 
 echo; echo Extracting:
