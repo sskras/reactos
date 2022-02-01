a286bc1326c469faff3efe67206e0a811b60c375
Saulius Krasuckas <saulius2@gmail.com>
2022-02-01 22:20:21 +0200

    build-vbox-OVA-from-ISO.sh: Prepare for another, related variable

--- build-vbox-OVA-from-ISO.sh
+++ build-vbox-OVA-from-ISO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ISO_URL="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
+ISO_URL_="https://sourceforge.net/projects/reactos/files/ReactOS/0.4.14/ReactOS-0.4.14-iso.zip/download?use_mirror=netix"
 
-wget -nv --show-progress -c --content-disposition ${ISO_URL}
+wget -nv --show-progress -c --content-disposition ${ISO_URL_}
